51de8d5757612da1ef8240e6350ededab1db78f0
Baohua Gao <wakaranaino@users.noreply.huggingface.co>
2026-04-09 14:42:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 import gradio as gr
+import requests
+import os
 
-def greet(name):
-    return "Hello " + name + "!"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+def call_llm(prompt):
+    url = "https://api.groq.com/openai/v1/chat/completions"
+
+    headers = {
+        "Authorization": f"Bearer {GROQ_API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    data = {
+        "model": "llama-3.1-8b-instant",
+        "messages": [
+            {"role": "user", "content": prompt}
+        ]
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+    result = response.json()
+
+    return result["choices"][0]["message"]["content"]
 
 demo = gr.Interface(
-    fn=greet,
+    fn=call_llm,
     inputs="text",
     outputs="text"
 )
